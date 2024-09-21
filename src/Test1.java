f96ce438e2a1994cb5c383ede93d8e9c094fbee0
venkatasateeshqa <158146037+venkatasateeshqa@users.noreply.github.com>
2024-09-21 12:56:14 +0530

    Testing for Squash (#5)

--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test 1");
         System.out.println("Test 4");
+        System.out.println("For test purpose");
     }
 }
